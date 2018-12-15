b5d726b3e23794b79396288b9f38d804ea7d5569
siyuzhu <jasonchu13@outlook.com>
2018-12-15 09:54:06 -0600

    auto feature engineering

--- preprocess/read_data.py
+++ preprocess/read_data.py
@@ -8,9 +8,14 @@ Created on Tue Dec 11 16:32:58 2018
 import os
 import pandas as pd
 import numpy as np
+import random
+from collections import deque
+import featuretools as ft
 
-def read_data(data_dir="/content/EloMerchantKaggle/data",
-            version = 'default'):
+import copy
+
+def read_data_z(data_dir="/content/EloMerchantKaggle/data",
+            version = '2',debug = False):
     """
     """
     print("loading train.csv ..." )
@@ -27,11 +32,28 @@ def read_data(data_dir="/content/EloMerchantKaggle/data",
 
     target_col = "target"
 
-    train_df_stat, test_df_stat = addStatFeatures(train_df,test_df,hist_df,new_trans_df)
+    if debug:
+        train_df_debug = pd.DataFrame(columns=train_df.columns)
+        hist_df_debug = pd.DataFrame(columns=hist_df.columns)
+        new_trans_df_debug = pd.DataFrame(columns=new_trans_df.columns)
+        new_models = [train_df_debug,hist_df_debug,new_trans_df_debug]
+        old_models = [train_df,hist_df,new_trans_df]
+        seeds = [5,23,43]
+        for i,data in enumerate(old_models):
+            random.seed(seeds[i])
+            sample_index = random.sample(range(data.shape[0]),int(data.shape[0]/4))
+            for j in sample_index:
+                new_models[i].append(data.iloc(j))
+            del data
+        train_df = train_df_debug.copy()
+        hist_df = hist_df_debug.copy()
+        new_trans_df = new_trans_df_debug.copy()
 
-    if version != '' and version.strip()[0] == 'c':
+
+    if version != '' and version.strip()[0] == '1':
         pass
-    elif version != '' and version.strip()[0] == 'z':
+    elif version != '' and version.strip()[0] == '2':
+        train_df_stat, test_df_stat = add_stat_features(train_df, test_df, hist_df, new_trans_df)
         # Feature engineering: polynomial features
         poly_features = train_df_stat[['feature_1','feature_2','feature_3']]
         poly_features_test = test_df_stat[['feature_1','feature_2','feature_3']]
@@ -73,16 +95,11 @@ def read_data(data_dir="/content/EloMerchantKaggle/data",
 
     else:
         # default only use statistic features
-        cols_to_use = ["feature_1", "feature_2", "feature_3", "year", "month",
-                       "num_hist_transactions", "sum_hist_trans", "mean_hist_trans", "std_hist_trans",
-                       "min_hist_trans", "max_hist_trans",
-                       "num_merch_transactions", "sum_merch_trans", "mean_merch_trans", "std_merch_trans",
-                       "min_merch_trans", "max_merch_trans",
-                      ]
-        return train_df_stat[cols_to_use],  test_df_stat[cols_to_use],  train_df_stat[target_col].values
+        cols_to_use = ["feature_1", "feature_2", "feature_3"]
+        return train_df[cols_to_use],  test_df[cols_to_use],  train_df[target_col].values
 
 # add additional statistical features 
-def addStatFeatures(train_df,test_df,hist_df,new_trans_df):
+def add_stat_features(train_df,test_df,hist_df,new_trans_df):
     train_df_stat = train_df.copy(deep = True)
     test_df_stat = test_df.copy(deep=True)
 
@@ -133,6 +150,8 @@ def addStatFeatures(train_df,test_df,hist_df,new_trans_df):
     test_df_stat["month"] = test_df_stat["first_active_month"].dt.month
 
     return train_df_stat, test_df_stat
+
+
 
 if __name__ == "__main__":
     x_train, y_train, x_test = read_data(data_dir ="/Users/xavier.qiu/Documents/Kaggle/EloMerchantKaggle/data")
